6cb19b75cf1de992a4f4780ad263eadfdef8a79f
Junaid Bukhari <imjunaid1997@gmail.com>
2022-12-08 15:54:34 +0500

    scale dropdown

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,11 +18,12 @@ const Header = ({ scale, setScale }: props) => {
 
         <button
           className={styles.tooltip}
-          onClick={() =>
+          onClick={() => {
             document
               .getElementById(nodeStyle.dragable)!
-              .setAttribute('style', 'top:5em; left:0')
-          }
+              .setAttribute('style', 'top:5em; left:0');
+            setScale(100);
+          }}
         >
           <i className='fa-solid fa-location-arrow'></i>
           <span className={styles.tooltiptext}>Go to center</span>
@@ -30,7 +31,18 @@ const Header = ({ scale, setScale }: props) => {
         <button disabled={scale < 35} onClick={() => setScale(scale - 10)}>
           -
         </button>
-        <button className={styles.scale}>{scale}</button>
+        <div className={styles.dropdown}>
+          <button className={styles.scale}>{scale}%</button>
+          <div className={styles.dropdowncontent}>
+            {[40, 50, 60, 70, 80, 90, 100, 120, 140, 160, 180, 200].map(
+              (num) => (
+                <span onClick={() => setScale(num)}>
+                  {num}% {scale === num && <i className='fa fa-check'></i>}
+                </span>
+              )
+            )}
+          </div>
+        </div>
         <button disabled={scale > 900} onClick={() => setScale(scale + 10)}>
           +
         </button>
